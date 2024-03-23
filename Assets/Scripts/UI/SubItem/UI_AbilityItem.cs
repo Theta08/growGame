acf2410727b5bc8f691109b80a8e67f9157897bc
Theta08 <67591505+Theta08@users.noreply.github.com>
2024-03-23 20:18:19 +0900

    스텟 rank에 따라 색상 변경

--- Assets/Scripts/UI/SubItem/UI_AbilityItem.cs
+++ Assets/Scripts/UI/SubItem/UI_AbilityItem.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_AbilityItem : UI_Base
 {
@@ -10,13 +11,18 @@ public class UI_AbilityItem : UI_Base
         ChangeText,
         UpgradeMoneyText,
     }
-    
     enum Buttons
     {
         UpgradeButton
     }
+    enum Images
+    {
+        UI_AbilityItem,
+        Icon,
+    }
 
     Define.StatType _statType;
+    private Sprite _icon;
     private int _pay = 10;
     
     public override bool Init()
@@ -28,6 +34,7 @@ public class UI_AbilityItem : UI_Base
 
         BindText(typeof(Texts));
         BindButton(typeof(Buttons));
+        BindImage(typeof(Images));
         
         GetButton((int)Buttons.UpgradeButton).gameObject.BindEvent(OnUpgradeButton);
         
@@ -50,13 +57,61 @@ public class UI_AbilityItem : UI_Base
         _pay += Managers.Game.SaveData.SelectUpgradeCount(_statType);
         
         GetUpradeSetting();
+        OnImgSetting();
 
         GetText((int)Texts.TitleText).text = $"{_statType.ToString()} 증가";
         GetText((int)Texts.ChangeText).text =$"{GetUpradeSetting()}" ;
         GetText((int)Texts.UpgradeMoneyText).text = $"{_pay}";
 
+        GetImage((int)Images.Icon).sprite = _icon;
+
     }
 
+    void OnImgSetting()
+    {
+        string name = "";
+        string ColorStr = "";
+        int rank = 1;
+        Color color;
+        Sprite[] sprites = Resources.LoadAll<Sprite>("Sprites/UI/UI");
+        
+        switch (_statType)
+        {
+            case Define.StatType.MaxHp:
+                sprites = Resources.LoadAll<Sprite>("Sprites/UI/Props");
+                rank = Managers.Game.SaveData.MaxHpUpgrade.rank;
+                name = "Health";
+                break;
+            case Define.StatType.Attack:
+                rank = Managers.Game.SaveData.AttackUpgrade.rank;
+                name = "Select 0";
+                break;
+            case Define.StatType.Def:
+                rank = Managers.Game.SaveData.DefUpgrade.rank;
+                name = "Select 6";
+                break;
+        }
+        
+        foreach (var sprite in sprites)
+        {
+            if (sprite.name == name)
+                _icon = sprite;
+        }
+        
+        if (rank == 1)
+            // 하얀색에 가까운 주황색
+            ColorStr = "#F2BD85";
+        else if( rank == 2)
+            // 주황색
+            ColorStr = "#FF9600";
+        else if( rank == 3)
+            // 금색
+            ColorStr = "#FFBC00";
+        
+        ColorUtility.TryParseHtmlString(ColorStr, out color);
+        gameObject.GetComponent<Image>().color = color;
+    }
+    
     string GetUpradeSetting()
     {
         string currentStat = "";
